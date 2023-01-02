86e3a2bae48e2d8dd973d9d22db5bf1f9569c61d
Vitor <Vitor@DESKTOP-M1DFIR1>
2023-01-02 02:14:21 +0000

    Added Owner Controller + ListOwners command

---
+++ Application/Commands/ListOwners/ListOwnersCommand.cs
@@ -0,0 +1,14 @@
+﻿using Application.DTOs.Advert;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Commands.ListOwners;
+
+public sealed class ListOwnersCommand : IRequest<List<OwnerDTO>>
+{
+}
